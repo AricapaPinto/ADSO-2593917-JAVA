0a780179bb5b92dbdef9c3bac2344c7dc821cfaa
unknown <ADSO@PEARPRCSAFSD541.FormacionSena.RED>
2023-05-18 08:25:09 -0500

    Agreganfo scroll al registro usuarios

--- Registro Usuarios/src/Principal/Personas_registradas.java
+++ Registro Usuarios/src/Principal/Personas_registradas.java
@@ -12,11 +12,11 @@ public class Personas_registradas extends javax.swing.JFrame {
         initComponents();
         initAlternComponents();
 
-        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
-        jPanel2.setForeground(new java.awt.Color(0, 0, 0));
+        panel2.setBackground(new java.awt.Color(255, 255, 255));
+        panel2.setForeground(new java.awt.Color(0, 0, 0));
 
         // Establecer un GridLayout en jPanel2
-        jPanel2.setLayout(new GridLayout(0, 1));
+        panel2.setLayout(new GridLayout(0, 1));
 
     }
 
@@ -26,9 +26,10 @@ public class Personas_registradas extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        jPanel2 = new javax.swing.JPanel();
         jPanel3 = new javax.swing.JPanel();
         btn_regresar = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        panel2 = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
@@ -55,17 +56,6 @@ public class Personas_registradas extends javax.swing.JFrame {
                 .addGap(20, 20, 20)
                 .addComponent(jLabel1)
                 .addContainerGap(38, Short.MAX_VALUE))
-        );
-
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
-        );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 181, Short.MAX_VALUE)
         );
 
         btn_regresar.setBackground(new java.awt.Color(0, 153, 255));
@@ -95,21 +85,34 @@ public class Personas_registradas extends javax.swing.JFrame {
                 .addGap(15, 15, 15))
         );
 
+        javax.swing.GroupLayout panel2Layout = new javax.swing.GroupLayout(panel2);
+        panel2.setLayout(panel2Layout);
+        panel2Layout.setHorizontalGroup(
+            panel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 395, Short.MAX_VALUE)
+        );
+        panel2Layout.setVerticalGroup(
+            panel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 175, Short.MAX_VALUE)
+        );
+
+        jScrollPane1.setViewportView(panel2);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jScrollPane1)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 181, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
                 .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
@@ -129,7 +132,7 @@ public class Personas_registradas extends javax.swing.JFrame {
             
             if (this.ventana.listaPersonas[i] != null) {
                 JLabel label = new JLabel("Usuario " + i +" "+ this.ventana.listaPersonas[i].getNombres()+"\n || "+this.ventana.listaPersonas[i].getApellidos()+"\n || "+this.ventana.listaPersonas[i].getDireccion()+"\n=> "+this.ventana.listaPersonas[i].getTelefono()+"\n || "+this.ventana.listaPersonas[i].getEmail());
-                jPanel2.add(label);
+                panel2.add(label);
             }
         }
     }
@@ -139,7 +142,8 @@ public class Personas_registradas extends javax.swing.JFrame {
     private javax.swing.JButton btn_regresar;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JPanel panel2;
     // End of variables declaration//GEN-END:variables
 }
